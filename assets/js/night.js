4a4093e1d5c626707523b60781f0d81eb52de67b
felipe131305 <felipe08119617@gmail.com>
2022-02-04 09:55:30 -0500

    Se agrega el modo nocturno

---
+++ assets/js/night.js
@@ -0,0 +1,31 @@
+const btnSun = document.getElementById("id-sun");
+const btnNight = document.getElementById("id-moon");
+
+const sunMode = () => {
+  removingDarkMode("page", "dark-mode");
+  removingDarkMode("body", "dark-mode");
+  removingDarkMode("navNight", "dark-mode2");
+  document.getElementById("id-moon").classList.remove("active");
+  this.classList.add("active");
+};
+
+const removingDarkMode = (id, className) => {
+  document.getElementById(id).classList.remove(className);
+};
+
+const nightMode = () => {
+  removingSunMode("page", "dark-mode");
+  removingSunMode("body", "dark-mode");
+  removingSunMode("navNight", "dark-mode2");
+  document.getElementById("id-sun").classList.remove("active");
+  this.classList.add("active");
+};
+const removingSunMode = (id, classMode) => {
+  document.getElementById(id).classList.add(classMode);
+};
+btnSun.onclick = function () {
+  sunMode();
+};
+btnNight.onclick = function () {
+  nightMode();
+};

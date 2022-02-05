67bb4ec9a130a946441c4938b87553b795d3b6eb
JulyMorales22 <jamorales859@misena.edu.co>
2022-02-05 17:36:44 -0500

    Se configura modo nocturno en accordion de question.html

--- assets/js/night.js
+++ assets/js/night.js
@@ -2,6 +2,7 @@ const btnSun = document.getElementById("id-sun");
 const btnNight = document.getElementById("id-moon");
 
 const sunMode = () => {
+  const numbers = ["One","Two","Three"];
   removingDarkMode("page", "dark-mode");
   removingDarkMode("body", "dark-mode");
   removingDarkMode("navNight", "dark-mode2");
@@ -12,6 +13,13 @@ const sunMode = () => {
   removingDarkMode("btnAccordion1", "dark-mode3");
   removingDarkMode("accordion2", "accordionContent");
   removingDarkMode("btnAccordion2", "dark-mode3");
+  numbers.forEach(i => {
+    removingDarkMode("btnA"+i,"dark-mode3");
+    removingDarkMode("collapse"+i,"accordionContent");
+  })
+  
+
+  
 
   nightModeActive = false;
 
@@ -20,10 +28,15 @@ const sunMode = () => {
 };
 
 const removingDarkMode = (id, className) => {
-  document.getElementById(id).classList.remove(className);
+  let dark = document.getElementById(id);
+  if(dark !=null){
+    dark.classList.remove(className);
+  }
+  // document.getElementById(id).classList.remove(className);
 };
 
 const nightMode = () => {
+  const numbers = ["One","Two","Three"];
   addingNightMode("page", "dark-mode");
   addingNightMode("body", "dark-mode");
   addingNightMode("navNight", "dark-mode2");
@@ -35,14 +48,23 @@ const nightMode = () => {
   addingNightMode("btnAccordion1", "dark-mode3");
   addingNightMode("accordion2", "accordionContent");
   addingNightMode("btnAccordion2", "dark-mode3");
-
+  numbers.forEach(i =>{
+  addingNightMode("btnA"+i,"dark-mode3");
+  addingNightMode("collapse"+i,"accordionContent");
+  })
+  
   nightModeActive = true;
 
   document.getElementById("id-sun").classList.remove("active");
   btnNight.classList.add("active");
 };
 const addingNightMode = (id, classMode) => {
-  document.getElementById(id).classList.add(classMode);
+  let night = document.getElementById(id);
+  if(night != null){
+    night.classList.add(classMode);
+  }
+
+  //  document.getElementById(id).classList.add(classMode);
 };
 btnSun.onclick = function () {
   sunMode();
